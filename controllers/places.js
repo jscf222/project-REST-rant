e02984335b342d2f5665dffb7f9050133dcded5b
jscf222 <jscf222@gmail.com>
2023-06-05 19:42:09 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -21,6 +21,21 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+//show
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
 //new
 
  router.get('/new', (req, res) => {
