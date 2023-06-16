610f107f9c5b5344fb8cf9671cf4b763b04300d9
jscf222 <jscf222@gmail.com>
2023-06-16 15:23:53 -0700

    mongodb

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,6 +3,7 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
+  console.log(data, 'data info')
     let placesFormatted = data.places.map((place) => {
       return (
         <div className= 'col-sm-6'>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,8 +7,35 @@ function show (data) {
         <Def>
           <main>
             <h1>{data.place.name}</h1>
+            <h3>Located in {data.place.city},{data.place.satate}</h3>
         <h2>Rating</h2>
+       
     <p> Not Rated</p>
+     
+  
+    
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+       
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+     
+    </div>
+  
+
+
     <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit </a>  
         <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
              <button type="submit" className="btn btn-danger">Delete</button>
