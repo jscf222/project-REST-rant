22ac84d04ba43b996fd33fb5abef793f899e17ad
jscf222 <jscf222@gmail.com>
2023-06-25 20:59:59 -0700

    render

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require ('method-override')
+const mongoose = require('mongoose')
 //configuration
 app.get('/', (req, res) => {
     res.render('home')
@@ -16,7 +17,10 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 
 app.use('/places', require('./controllers/places'))
-
+const PORT = process.env.PORT || 3000
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
 
 app.get('*', (req, res) => {
     res.render('error404')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,11 +3,16 @@ const Def = require('../default')
 const comments = require("../../models/comment")
 
 function show(data) {
-  let comments = <h3 className="inactive">No comments yet!</h3>;
-  let rating = <h3 className="inactive">Not yet rated</h3>;
+  let comments = <h3 className="inactive">No comments yet!</h3>
+  let rating = <h3 className="inactive">Not yet rated</h3>
   if (data.place.comments.length) {
     comments = data.place.comments.map((c) => {
       //add ratings
+      
+          
+              
+             
+      
       let sumRatings = data.place.comments.reduce((total, comment) => {
         return total + comment.stars
       }, 0)
@@ -20,6 +25,7 @@ function show(data) {
       rating = <h3>{stars} stars</h3>
       return (
         <div className="col-sm-4 rant-box">
+          
           <h2 className="rant">{c.rant ? "Rant! :(" : "Rave! :)"}</h2>
           <h4>{c.content}</h4>
           <h3>
@@ -42,7 +48,7 @@ function show(data) {
   }
 
   return (
-   <Def>
+    <Def>
       <main>
         <div className="row">
           <div className="col-sm-6">
@@ -61,19 +67,19 @@ function show(data) {
               <h2>Rating</h2>
               {rating}
               <br />
-              
-              
+
+
             </div>
             <div className='main' style={{ color: 'lightblue' }}>
               <h2>Description</h2>
               <div className='main' style={{ color: 'black' }}>
                 <h3>{data.place.showEstablished()}</h3>
-              <h4>Serving {data.place.cuisines} </h4>
-            </div>
+                <h4>Serving {data.place.cuisines} </h4>
+              </div>
             </div>
             <a href={`/places/${data.place.id}/edit`}
               className="btn btn-warning">
-                Edit</a>
+              Edit</a>
             <form
               method="POST" action={`/places/${data.place.id}/?_method=DELETE`}
             >
@@ -86,84 +92,84 @@ function show(data) {
           <div className="row">
             <h2>Comments</h2>
             {comments}
-            <hr/>
-            <div className = 'rant-post-box'>
-            <h1>Got Your Own Rant or Rave?</h1>
+            <hr />
+            <div className='rant-post-box'>
+              <h1>Got Your Own Rant or Rave?</h1>
               <form
                 className="row g-3"
                 method="POST"
                 action={`/places/${data.place.id}/comment`}
               >
-               <div className="reviewForm"> 
-                <div className="col-12">
-                  <label htmlFor="content" className="form-label">
-                    Your Comment
-                  </label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    id="content"
-                    name="content"
-                  />
-                </div>
-                <div className="col-md-4">
-                  <label htmlFor="author" className="form-label">
-                    Your Name
-                  </label>
-                  <input className="form-control" id="author" name="author" />
-                </div>
-                <div className="col-md-4">
-                  <label htmlFor="stars" className="form-label">
-                    Stars
-                  </label>
-                  <input
-                    className="form-range"
-                    type="range"
-                    id="stars"
-                    name="stars"
-                    min="1"
-                    max="5"
-                    step="0.5"
-                  />
-                </div>
-                <div className="col-md-2">
-                  <div className="form-check">
-                    <label
-                      class="form-check-label"
-                      htmlFor="rant"
-                      id="rant-checkbox"
-                    >
-                      Rant?
+                <div className="reviewForm">
+                  <div className="col-12">
+                    <label htmlFor="content" className="form-label">
+                      Your Comment
                     </label>
-                    <br />
                     <input
-                      className="form-check-input"
-                      type="checkbox"
-                      id="rant"
-                      name="rant"
+                      className="form-control"
+                      type="text"
+                      id="content"
+                      name="content"
+                    />
+                  </div>
+                  <div className="col-md-4">
+                    <label htmlFor="author" className="form-label">
+                      Your Name
+                    </label>
+                    <input className="form-control" id="author" name="author" />
+                  </div>
+                  <div className="col-md-4">
+                    <label htmlFor="stars" className="form-label">
+                      Stars
+                    </label>
+                    <input
+                      className="form-range"
+                      type="range"
+                      id="stars"
+                      name="stars"
+                      min="1"
+                      max="5"
+                      step="0.5"
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    <div className="form-check">
+                      <label
+                        class="form-check-label"
+                        htmlFor="rant"
+                        id="rant-checkbox"
+                      >
+                        Rant?
+                      </label>
+                      <br />
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="rant"
+                        name="rant"
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      className="btn btn-primary"
+                      type="submit"
+                      value="Add Comment"
                     />
                   </div>
                 </div>
-                <div className="form-group">
-                  <input
-                    className="btn btn-primary"
-                    type="submit"
-                    value="Add Comment"
-                  />
-                </div>
-               </div>
-               
+
               </form>
             </div>
           </div>
         </div>
       </main>
     </Def>
-   )
-  }
+  )
+}
 
 module.exports = show
-            {/* </div>
+{/* </div>
           </div>
 
         </div>
